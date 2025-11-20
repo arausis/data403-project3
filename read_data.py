bea19219247b99f48aeb9c29c1d13f22037ec5bd
arausis <anselrausis@gmail.com>
2025-11-20 13:45:04 -0800

    added a basic system for manually tagging images

--- read_data.py
+++ read_data.py
@@ -10,17 +10,19 @@ import matplotlib.pyplot as plt
 def get_dirs():
     path = os.path.dirname(os.path.abspath(__file__))
 
-    alex_pics = [f"{path}/data/Alex/{fname}" for fname in os.listdir(f"{path}/data/Alex")]
+    alex_files = os.listdir(f"{path}/data/Alex")
+    alex_pics = [f"{path}/data/Alex/{fname}" for fname in alex_files]
     alabel = [0] * len(alex_pics)
 
-    kelly_pics = [f"{path}/data/Kelly/{fname}" for fname in os.listdir(f"{path}/data/Kelly")]
+    kelly_files = os.listdir(f"{path}/data/Kelly")
+    kelly_pics = [f"{path}/data/Kelly/{fname}" for fname in kelly_files]
     klabel = [1] * len(kelly_pics)
 
-    df = pd.DataFrame({"label": alabel + klabel, "path" : alex_pics + kelly_pics})
+    df = pd.DataFrame({"fname" : alex_files + kelly_files, "label": alabel + klabel, "path" : alex_pics + kelly_pics})
     return df
 
 def read_image(image_path):
-    image = Image.open(image_path)
+    image = Image.open(image_path).convert("RGB")
     image_array = np.array(image)
     return image_array
 
@@ -28,7 +30,36 @@ if __name__ == "__main__":
     df = get_dirs()
     df["image"] = df["path"].map(read_image)
 
-    image_array = df["image"].iloc[0]
+    manual_columns = ["q1", "q2", "q3", "q4"]
 
-    plt.imshow(image_array)
-    plt.show()
+    manual_input = []
+
+    for file in list(df["fname"]):
+        image_array = df[df["fname"].map(lambda x: x == file)]["image"].iloc[0]
+
+        plt.imshow(image_array)
+        plt.show(block=False)
+
+        input_map = {}
+
+        # Record the input for each column
+        print()
+        for col in manual_columns:
+            input_map[col] = int(input(f"Y/n | {col} : ").strip().lower() == "y")
+
+        manual_input.append(input_map)
+
+        if len(manual_input) == 3:
+            break
+
+        plt.close()
+    
+    # Reformat the inputs so that we can make a dataframe
+    df_dict = {}
+    df_dict["fname"] = list(df["fname"])[:3]
+
+    for col in manual_columns:
+        df_dict[col] = [x[col] for x in manual_input]
+
+    input_df = pd.DataFrame(df_dict)
+    input_df.to_csv("manual_tagging.csv")
